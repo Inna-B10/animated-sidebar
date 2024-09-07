c736e92a714a8919d2e0dfbbaeded50ac73f9a7b
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-09-07 14:59:37 +0200

    added visual toggle button

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,9 +1,19 @@
+import { PanelLeftClose, PanelLeftOpen } from 'lucide-react'
+import { useState } from 'react'
 import Menu from './menu/Menu'
 import styles from './Sidebar.module.scss'
 
 export default function Sidebar() {
+	const [isCollapsed, setIsCollapsed] = useState(false)
+
+	const toggleSidebar = () => {
+		setIsCollapsed(!isCollapsed)
+	}
 	return (
 		<aside className={styles.sidebar}>
+			<button className={styles.toggle} onClick={toggleSidebar}>
+				{isCollapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
+			</button>
 			<Menu />
 		</aside>
 	)
