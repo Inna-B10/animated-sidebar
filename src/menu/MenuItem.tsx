1b592b67f8d5ba28ac18c7af771094aff68cb35e
Inna-B10 <inna.b10.kodehode@gmail.com>
2024-09-07 16:15:31 +0200

    link target='_blank'

--- src/menu/MenuItem.tsx
+++ src/menu/MenuItem.tsx
@@ -5,7 +5,7 @@ import { IMenuItem } from './menu.data'
 export default function MenuItem({ item }: { item: IMenuItem }) {
 	const isCollapsed = useAtomValue(isCollapsedAtom)
 	return (
-		<a href={item.link} rel='noreferrer noopener'>
+		<a href={item.link} rel='noreferrer noopener' target='_blank'>
 			<item.icon />
 			{!isCollapsed && <span>{item.name}</span>}
 		</a>
